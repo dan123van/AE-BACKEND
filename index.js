289084a45a01cc97146e0c922e5ec8ddf2d55938
Daniel Silverio <danielsf123van@gmail.com>
2023-05-03 20:04:08 -0600

    Se agregaron cambios a las rutas

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 require('dotenv').config({ path: __dirname + '/.env' })
 require('./setup/express.js')(app)
 
-app.listen(3000)
+app.listen(3001)
--- src/routes/admin-route.js
+++ src/routes/admin-route.js
@@ -2,14 +2,24 @@
 
 const router = (app) => {
 
-    app.get('/admin', function (req, res) {
-        console.log(`Ya estoy en mi ruta GET ${req.params}`)
-        res.send('Hello World')
-    })
-
     app.post('/admin', function (req, res) {
         console.log(`Ya estoy en mi ruta POST ${req.body}`)
-        res.send('Hello World')
+        res.send('Post de admins')
+    })
+
+    app.get('/admin/:id', function (req, res) {
+        console.log(`Ya estoy en mi ruta GET ${req.params.id}`)
+        res.send('Get de admins')
+    })
+
+    app.put('/admin', function (req, res) {
+        console.log(`Estoy en una modificacion`)
+        res.send('Get de admins')
+    })
+
+    app.delete('/admin', function (req, res) {
+        console.log(`Estoy en una seccion`)
+        res.send('Get de admins')
     })
 
 }
