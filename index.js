ce1d0ac65281ee7561b2156c5580c2b4b2933e11
Daniel Silverio <danielsf123van@gmail.com>
2023-05-04 20:58:12 -0600

    Se agrego el endpoint test, tambien se agregdo body-parser, cors y morgan como dependencias

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true}))
 
 require('dotenv').config({ path: __dirname + '/.env' })
 require('./setup/express.js')(app)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,23 @@
+const cors = require('cors')
 let admin = require('./admin-route')
+let test = require('./test-route')
 
 let routes = (app) => {
+    app.options('*', cors());
+
+    app.all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Headers', 
+        'X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Methods', 
+        'GET, POST, PUT, DELETE, OPTIONS');
+
+        next()
+    });
+
     try {
         admin(app)
+        test(app)
     } catch (error) {
         console.log(error);
     }
